adbc24cb695c000a912cd2896440ad61b0ba6eb8
Andrei Tatarinov <andrei.tatarinov@mail.ru>
2026-08-03 01:12:54 +0700

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+
+  <div class="app-layout">
+
+    <header class="header">
+
+      <h2>
+        Smart Surveillance Platform
+      </h2>
+
+    </header>
+
+
+    <main class="content">
+
+      <router-view />
+
+    </main>
+
+  </div>
+
+</template>
